f9d9d827bf7395f540047b44d48e1c336e707956
Sashusha-kuskus <99893714+Sashusha-kuskus@users.noreply.github.com>
2022-02-22 15:06:33 +0300

    Add files via upload

---
+++ ЛР 13-14/Z1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Z1
+{
+    class EventKeyArgs : EventArgs //аргументы для события
+    {
+        public ConsoleKeyInfo cki;
+    }
+
+    delegate void KeyHalder(object source, EventKeyArgs arg); //объявление делегата
+
+    class KeyEvent //класс события
+    {
+        public event KeyHalder KeyPress; //объявление события
+
+        public void OnPressKey(ConsoleKeyInfo ch) //событие
+        {
+            EventKeyArgs a = new EventKeyArgs();
+            if (KeyPress != null)
+            {
+                a.cki = ch;
+                KeyPress(this, a);
+            }
+        }
+    }
+
+    class WriteInf
+    {
+        public void WriteInfo(object source, EventKeyArgs arg) //обработчик события
+        {
+            Console.WriteLine("{0}, Символ: {1}", arg.cki.Key, arg.cki.KeyChar);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyEvent ev = new KeyEvent();
+            WriteInf wr = new WriteInf();
+            ConsoleKeyInfo cki;
+            string Str = "";
+            ev.KeyPress += new KeyHalder(wr.WriteInfo); //добавление обработчика события
+
+            Console.WriteLine("Введите несколько символов. \nДля останова нажмите F1.");
+            do
+            {
+                Console.Write("Введите символ.");
+                cki = Console.ReadKey(true);
+                ev.OnPressKey(cki); //запуск события
+                Str += cki.KeyChar;
+            } while (cki.Key != ConsoleKey.F1);
+        }
+    }
+}
---
+++ ЛР 13-14/Z2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Z2
+{
+    delegate void Changing(); //делегат
+
+    class EventClass
+    {
+        public event Changing ChangeName; //объявление события
+
+        public void OnChange() //событие
+        {
+            if (ChangeName != null)
+            {
+                ChangeName();
+            }
+        }
+    }
+
+    class WriteInf
+    {
+        public void PrintName1() //обработчик события
+        {
+            Console.WriteLine("Название изменилось!");
+        }
+        public void PrintName2() //обработчик события
+        {
+            Console.WriteLine("Название не изменилось!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameAuto = "VOLKSWAGEN";
+            EventClass ev = new EventClass();
+            WriteInf wr = new WriteInf();
+
+            Console.WriteLine("Название автомобиля: {0}", nameAuto);
+
+            Console.WriteLine("Изменить название автомобиля? Y - да; Остальные символы - нет");
+            ConsoleKeyInfo cki;
+            cki = Console.ReadKey(true);
+            if (cki.Key == ConsoleKey.Y)
+            {
+                Console.Write("Введите новое название: ");
+                string newName = Console.ReadLine();
+                nameAuto = newName;
+                ev.ChangeName += new Changing(wr.PrintName1); //добавление обработчика события
+                ev.OnChange(); //запуск события
+            }
+            else
+            {
+                ev.ChangeName += new Changing(wr.PrintName2); //добавление обработчика события
+                ev.OnChange(); //запуск события
+            }
+
+            Console.WriteLine("Название автомобиля: {0}", nameAuto);
+        }
+    }
+}
